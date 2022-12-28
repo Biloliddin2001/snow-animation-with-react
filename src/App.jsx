066c807e522d915680023479d6305b9ea8ca1587
bilolsh <bilolsh01@gmail.com>
2022-12-28 08:44:27 +0300

    add new style

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React, { Component } from "react";
-import Sneg from "./assets/sneg.jpg";
+import React from "react";
+import Snowfall from "react-snowfall";
+import photo from "./assets/snow-2.png";
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <div className="snow_wrap">
-          <div className="snow"></div>
-          <h2>Sh.Biloliddin</h2>
-          <p>26.12.2022</p>
-        </div>
-      </>
-    );
-  }
-}
+const App = () => {
+  const snowFlake = document.createElement("img");
+  snowFlake.src = photo;
+
+  const images = [snowFlake];
+  return (
+    <>
+      <div className="snowfal">
+        <Snowfall
+          snowflakeCount={200}
+          images={images}
+          radius={[10, 20]}
+          speed={[0.9, 3]}
+          wind={[0.5, 1.5]}
+        />
+      </div>
+    </>
+  );
+};
+export default App;
